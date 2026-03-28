3d855f7c5d9bc0f935cea50a2500ed827d0ec740
paultramalloni-lab <paul.tramalloni@gmail.com>
2026-03-28 14:44:31 +0100

    Fix env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://bzruqthdxgnsgdbgatqy.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6cnVxdGhkeGduc2dkYmdhdHF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2OTMwNzQsImV4cCI6MjA5MDI2OTA3NH0.Zl3HSh2QT1AyAJh-_xTIUc2IRs-jZRO-yvNE38RXoA8*'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
